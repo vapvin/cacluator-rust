c000efded097f2e99d25e2a5271f52621fe68395
vapvin <vins@vins.dev>
2020-08-29 03:36:22 +0900

    read fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{stdin, stdout, Write};
+
+fn read(input: &mut String){
+    stdout().flush()
+        .expect("Failed to Flush");
+    stdin().read_line(input)
+        .expect("Failed to Read");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("심플 계산기");
+    println!("-------------------");
+
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    let mut operator = String::new();
+
+    print!("첫 번째 숫자: ");
 }
